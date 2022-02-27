f0e68aab3dc6160706e242f3570252de90a25659
Jocelyn Raymond <it.jraymond@gmail.com>
2022-02-27 11:38:14 -0700

    Initial setup

---
+++ src/main/java/ca/jent/payload/PayloadApplication.java
@@ -0,0 +1,13 @@
+package ca.jent.payload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/jent/payload/PayloadApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jent.payload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
